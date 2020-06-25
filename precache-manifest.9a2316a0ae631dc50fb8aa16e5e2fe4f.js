57b508d2e06541c3024b2fa604a39046c375bb4c
bergmaner <bergkacper@gmail.com>
2020-06-25 10:21:11 +0200

    Updates

---
+++ precache-manifest.9a2316a0ae631dc50fb8aa16e5e2fe4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c946b6c77c3ffca7a5a42f3d83bd7399",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "8e18cb70f8f1a2ec356b",
+    "url": "/tetris/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8df985c7fdff067d1500",
+    "url": "/tetris/static/js/2.ff03a8e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tetris/static/js/2.ff03a8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e18cb70f8f1a2ec356b",
+    "url": "/tetris/static/js/main.671f419f.chunk.js"
+  },
+  {
+    "revision": "c179afea1aa9726cb16b",
+    "url": "/tetris/static/js/runtime-main.0d8747ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tetris/static/media/logo.5d5d9eef.svg"
+  }
+]);
