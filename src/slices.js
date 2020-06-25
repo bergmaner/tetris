baafb92474aebaab1099966cbac70501a61b6611
bergmaner <bergkacper@gmail.com>
2020-06-25 14:23:49 +0200

    generate random Slice

---
+++ src/slices.js
@@ -0,0 +1,64 @@
+export const Slices = {
+  0: { shape: [[0]], color: "0, 0, 0" },
+  I: {
+    shape: [
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+      [0, "I", 0, 0],
+    ],
+    color: "90, 230, 230",
+  },
+  J: {
+    shape: [
+      [0, "J", 0],
+      [0, "J", 0],
+      ["J", "J", 0],
+    ],
+    color: "40, 100, 220",
+  },
+  L: {
+    shape: [
+      [0, "L", 0],
+      [0, "L", 0],
+      [0, "L", "L"],
+    ],
+    color: "220, 170, 40",
+  },
+  O: {
+    shape: [
+      ["O", "O"],
+      ["O", "O"],
+    ],
+    color: "220, 220, 240",
+  },
+  S: {
+    shape: [
+      [0, "S", "S"],
+      ["S", "S", 0],
+      [0, 0, 0],
+    ],
+    color: "50, 210, 55",
+  },
+  T: {
+    shape: [
+      [0, 0, 0],
+      ["T", "T", "T"],
+      [0, "T", 0],
+    ],
+    color: "130, 60, 200",
+  },
+  Z: {
+    shape: [
+      ["Z", "Z", 0],
+      [0, "Z", "Z"],
+      [0, 0, 0],
+    ],
+    color: "225, 80, 80",
+  },
+};
+export const randomSlice = () => {
+  const slices = "IJLOSTZ";
+  const randSlice = slices[Math.floor(math.random() * slices.length)];
+  return slices[randSlice];
+};
